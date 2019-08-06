d8abf82aabbb410833471f59ac748443cec25d36
linkaitao <linkaitao@qiku.com>
2019-08-06 10:07:38 +0800

    update implement

--- kth_largest_ele_in_an_array/kth_largest_ele.go
+++ kth_largest_ele_in_an_array/kth_largest_ele.go
@@ -1,13 +1,40 @@
 package kth_largest_ele_in_an_array
 
-import "sort"
+import (
+	"container/heap"
+)
 
 func findKthLargest(nums []int, k int) int {
-	if len(nums) == 0 || k > len(nums) {
-		return 0
+	temp := make([]int, k)
+	//copy(temp, nums[:k])
+	for i := 0; i < k; i++ {
+		temp[i] = nums[i]
 	}
-	temp := make([]int, len(nums))
-	copy(temp, nums)
-	sort.Ints(temp)
-	return temp[len(temp)-k]
+	h := intHeap(temp)
+	heap.Init(&h)
+
+	for i := k; i < len(nums); i++ {
+		if nums[i] > h[0] {
+			h[0] = nums[i]
+			heap.Fix(&h, 0)
+		}
+	}
+	return h[0]
 }
+
+type intHeap []int
+
+func (h intHeap) Len() int {
+	return len(h)
+}
+
+func (h intHeap) Less(i, j int) bool {
+	return (h)[i] < (h)[j]
+}
+
+func (h intHeap) Swap(i, j int) {
+	(h)[i], (h)[j] = (h)[j], (h)[i]
+}
+
+func (intHeap) Push(x interface{}) {}
+func (intHeap) Pop() interface{}   { return nil }
--- merge_k_sorted_lists/merge_k_sorted_lists.go
+++ merge_k_sorted_lists/merge_k_sorted_lists.go
@@ -1,66 +1,63 @@
 package merge_k_sorted_lists
 
+import "container/heap"
+
 //Definition for singly-linked list.
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
-type HeapNode struct {
-	N     *ListNode
-	Left  *HeapNode
-	Right *HeapNode
-	LHigh int
-	RHigh int
-}
-
-func (h *HeapNode) Push(n *ListNode) {
-	if h == nil {
-		return
-	}
-	if n.Val < h.N.Val{
-
-	}
-}
-
-func (h *HeapNode) Pop() (n *ListNode) {
-
-}
-
 func mergeKLists(lists []*ListNode) *ListNode {
-	if lists == nil {
+	if len(lists) < 1 {
 		return nil
 	}
-	var minNode *ListNode
-	var k int
-	var sorted *ListNode
-	var last *ListNode
-	for {
-		minNode = nil
-		k = -1
 
-		for i, node := range lists {
-			if node != nil {
-				if minNode == nil {
-					minNode = node
-					k = i
-				} else if minNode.Val > node.Val {
-					k = i
-					minNode = node
-				}
-			}
+	var h nodeHeap
+	for i := 0; i < len(lists); i++ {
+		if lists[i] != nil {
+			h = append(h, lists[i])
 		}
-		if k == -1 {
+	}
+	if len(h) < 1 {
+		return nil
+	}
+	heap.Init(h)
+	var last = h[0]
+	var sorted = last
+	for {
+		h[0] = h[0].Next
+		if h[0] == nil {
+			h = heap.Pop(h).(nodeHeap)
+		}
+		if len(h) < 1 {
 			break
 		}
-		lists[k] = lists[k].Next
-		minNode.Next = nil
-		if sorted == nil {
-			sorted = minNode
-		} else {
-			last.Next = minNode
+		if len(h) == 1 {
+			last.Next = h[0]
+			break
 		}
-		last = minNode
+		heap.Fix(h, 0)
+
+		last.Next = h[0]
+		last = last.Next
 	}
 	return sorted
 }
+
+type nodeHeap []*ListNode
+
+func (h nodeHeap) Len() int {
+	return len(h)
+}
+
+func (h nodeHeap) Less(i, j int) bool {
+	return h[i].Val < h[j].Val
+}
+
+func (h nodeHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (nodeHeap) Push(x interface{}) {}
+func (h nodeHeap) Pop() interface{} { return h[:len(h)-1] }
